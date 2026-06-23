3ed0201ea486e79a6f6819a3c4f9df655005afe9
Troy Frever <troyf@pobox.com>
2026-06-22 17:08:52 -0700

    Update run configs, scripts, gitignore, and add whidbey node data

--- run_debug.sh
+++ run_debug.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# this script needs to be run from the root of the repository, since paths are relative to that
+
 set -e
 
 # use --verbose on the build to see everything
 cmake --build ./build/debug-hack --target headless; cp troy_run_listings_template.csv \
-  troy_run_listings.csv; date; ./bin/Debug/headless "troy_run_listings.csv" "run32/map2000d2014" \
-  "run29_config/config_map_2000_data_2014_deterministic.json"; date
+  troy_run_listings.csv; date; ./bin/Debug/headless "troy_run_listings.csv" "data/whidbey/output" \
+  "data/whidbey/config/config_map_2000_data_2014_deterministic.json"; date
