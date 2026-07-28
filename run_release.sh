b99d252c99b1fe4a096d75be1e1e964684bc11eb
Troy Frever <troyf@pobox.com>
2026-07-28 10:53:53 -0700

    make run_release.sh

---
+++ run_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this script needs to be run from the root of the repository, since paths are relative to that
+
+set -e
+
+# use --verbose on the build to see everything
+cmake --build ./build/cmake-release --target headless; cp troy_run_listings_template.csv \
+  troy_run_listings.csv; date; ./bin/Release/headless "troy_run_listings.csv" "data/whidbey/output" \
+  "data/whidbey/config/config_map_2000_data_2014.json"; date
